9c884bf7012eab71113c4e382e1563eca8d20941
Halfdan Karlsen Monie <half0150@edu.sde.dk>
2024-02-12 12:49:29 +0100

    ArrayRotation

---
+++ ArrayRotation/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ArrayRotation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rotatedArray = ArrayRotation(new int[] { 3, 8, 9, 7, 6 }, 1);
+            Console.WriteLine(string.Join(", ", rotatedArray)); // For at udskrive det roterede array
+        }
+
+        static int[] ArrayRotation(int[] A, int K)
+        {
+            int N = A.Length;
+            int[] rotatedArray = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                int newIndex = (i + K) % N; // Beregning af det nye indeks efter rotation
+                rotatedArray[newIndex] = A[i]; // Indsætter værdien på det nye indeks
+            }
+
+            return rotatedArray;
+        }
+    }
+}
